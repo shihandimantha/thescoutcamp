bad7d384bbe5b4c7d3a47c4a48589c40f07421da
shihandimantha <76395781+shihandimantha@users.noreply.github.com>
2022-02-20 07:56:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+<script src="./removeBanner.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peace Day 2021</title>
+    <link rel="shortcut icon" href="image/logo.jpg" type="image/x-icon">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css1/style.css">
+    <style>img[alt="www.000webhost.com"]{display:none;}</style>
+
+</head>
+<body>
+    
+<!-- gradeint background  -->
+
+<div class="gradient-background"></div> 
+
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <i class="fas fa-dove"></i> PEACE DAY 2021 </a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#Activities">Activities</a>
+        <a href="image/badge.png" download="badge">Badge</a>
+        <a href="login/login.php">Signup/Login</a>
+        <a href="game/dovesignsymbol.php">Game</a>
+        <a href="nationalevents.php" target="_blank">National events</a>
+        
+        
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3 class="glow">PEACE DAY 2021</h3>
+
+        <p>Recovering better for an equitable and sustainable<br> world</p>
+       
+        
+    </div>
+   
+    
+   
+  
+</section>
+
+<!-- home section ends -->
+
+<!-- courses section starts  -->
+
+<section class="Activities" id="Activities">
+
+    <h1 class="heading"> <span>Activities</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <a href="login/index.php"><img src="image/poster.png" alt=""></a>
+            <h3>Poster Competetion</h3>
+            <p>Send a digital poster or hand drawn poster by 20</p>
+            
+            
+        </div>
+
+        <div class="box">
+        <a href="login/index.php"><img src="image/wordsearch1.png" alt=""></a>
+            <h3> Word Search </h3>
+            <p>Word search game in peace day 2021</p>
+            
+        </div>
+
+        <div class="box">
+            <a href="login/index.php"><img src="image/un.png" alt=""></a>
+            <h3>International Day of Peace Youth Observance</h3>
+            <p>Register for International Day of Peace Youth Observance</p>
+            
+        </div>
+
+        <div class="box">
+            <a href="login/index.php"><img src="image/sdg.jpg" alt=""></a>
+            <h3>Quiz on SDGs</h3>
+            <p>Quiz on SDGs and sri lanka scouting history</p>
+        </div>
+        <div class="box">
+            <a href="login/index.php"><img src="image/dove2.png" alt=""></a>
+            <h3>Image Gallery</h3>
+            <p>Gallery</p>
+        </div>
+        <div class="box">
+            <a href="login/index.php"><img src="image/quote.png" alt=""></a>
+            <h3>Quotes</h3>
+            <p>By legends on peace</p>
+        </div>
+
+
+
+      
+        
+
+    </div>
+
+</section>
+
+<!-- courses section ends -->
+
+  
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom javascript file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ nationalevents.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="./removeBanner.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>National Events</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css1/style3.css">
+    <style>img[alt="www.000webhost.com"]{display:none;}</style>
+</head>
+<body background="darkviolet">
+    <div class="topic">
+<center></b> <u><h3 size="10px">ජාත්‍යන්තර සාම දිනය (IDP) - 2021</h3></u></b></center>
+<p>සෑම වසරකම සැප්තැම්බර් 21 වෙනිදා ජාත්‍යන්තර සාම දිනය ලොව පුරා සමරනු ලබයි.සාමයේ පරමාදර්ශ ශක්තිමත් කිරීම සඳහා කැප වූ දිනයක් එක්සත් ජාතීන්ගේ මහා මණ්ඩලය විසින් ප්‍රකාශයට පත් කර තිබේ.
+මෙවර තේමාව වී ඇත්තේ "Recovering better for an equitable and sustainable world" යන්නයි.
+</p>
+<div>
+
+</div>
+<center><u><h3>IDP 2021 වැඩසටහන - දිස්ත්‍රික් මට්ටම</h3></u></center>
+<p>
+ජාත්‍යන්තර සාම දිනය සඳහා නිවසේ සිට පහත සඳහන් කර ඇති ආකාරයට තරඟ වලට සහභාගී වීමට හැකිය.
+</p>
+
+
+
+
+
+    </div>
+    <div class="ok"><center>පෝතක අංශය - ඉවතන නෑ ද්‍රව්‍ය වලින් සෑදිය හැකි සාමය හා සම්බන්ධ අත්කම් නිර්මාණය(ප්ලාස්ටික්/කඩදාසි/පැළෑටි කොටස් ආදියෙන්)</center></div><br><hr>
+    <div><center>කනිෂ්ඨ බාලදක්ෂ අංශය ඉවත ලන ද්‍රව්‍ය වලින් සෑදිය හැකි සමය හා සම්බන්ධ අත්කම් නිර්මාණයක්(ප්ලාස්ටික්/කඩදාසි/පැළෑටි කොටස් ආදියෙන්)</center></div><br><hr>
+    <div><center>ජ්‍යේෂ්ඨ බාලදක්ෂ අංශය - "COVID - 19 වසංගතය හමුවේ ජාතියක් ලෙස නැඟී සිටිමු" යන මාතෘකාව යටතේ ඩිජිටල් පෝස්ටර් තරඟය(JPEG)</center></div><br><hr>
+    <div><center>මානවක බාලදක්ෂ අංශය - "COVID - 19 වසංගතය හමුවේ ජාතියක් ලෙස නැගී සිටිමු" යන මාතෘකාව යටතේ(JPEG) ඩිජිටල් වීඩියෝ සෑදීමේ තරඟය(මිනිත්තු 2-3 MP4 , තිරස් ආකාරයට , 720 p හෝ ඊට වැඩි වීඩියෝ ගුණාත්මකභාවය)</center></div><br><hr>
+    
+
+    
+</body>
+</html>
